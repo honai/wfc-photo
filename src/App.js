d8e959a0cebd0a5b47c0e84eb22c51b42629e5a5
honai <29170100+honai@users.noreply.github.com>
2019-03-10 00:49:08 +0900

    エラー・ローディングをリストの後ろに

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
-import ReactCssTransitionGroup from 'react-addons-css-transition-group';
 import PhotoDetail from './PhotoDetail';
 import PhotoList from './PhotoList';
 import './animation.css';
@@ -41,25 +40,21 @@ const App = () => {
     console.log(listSize);
   }
   
-  if (apiStatus.error) {
-    return <div>再度読み込みしてね</div>;
-  } else if (!apiStatus.isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <BrowserRouter>
-        <div>
-          <div><h3>フォト</h3></div>
-          <PhotoList imagesList={imagesList} onLinkClick={onLinkClick} />
-          <button onClick={gainListSize}>もっと読み込む</button>
+  return (
+    <BrowserRouter>
+      <div>
+        <div><h3>フォト</h3></div>
+        <PhotoList imagesList={imagesList} onLinkClick={onLinkClick} />
+        {apiStatus.isLoaded ? false : <div>Loading...</div>}
+        {apiStatus.error ? <div>再度読み込みしてね</div> : false}
+        <button onClick={() => gainListSize(10)}>もっと読み込む</button>
 
-          <Route path='/detail/:id' render={(props) => (
-            <PhotoDetail {...props} image={currentDetailStatus.imageInfo} />
-          )} />
-        </div>
-      </BrowserRouter>
-    );
-  }
+        <Route path='/detail/:id' render={(props) => (
+          <PhotoDetail {...props} image={currentDetailStatus.imageInfo} />
+        )} />
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
