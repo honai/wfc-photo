01f5a5fe84e664894d1107ec4f5682bb1b83a0db
honai <29170100+honai@users.noreply.github.com>
2019-03-09 16:37:32 +0900

    hooksに移行

--- src/App.js
+++ src/App.js
@@ -2,65 +2,49 @@ import React, { Component, useState, useEffect } from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
 import PhotoDetail from './PhotoDetail';
 import './style.css';
-// import PhotoList from './PhotoList';
+import PhotoList from './PhotoList';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-      isLoaded: false,
-      images: []
-    };
-  }
-
-  componentDidMount() {
-    fetch("https://wfc-2019.firebaseapp.com/images?limit=10&offset=0")
-      .then(res => res.json())
-      .then(
-        (result) => {
-          this.setState({
-            isLoaded: true,
-            images: result.data.images
-          });
+const App = () => {
+  const [getRange, setGetRange] = useState({limit: 10, offset: 0});
+  const [imagesList, setImagesList] = useState([]);
+  const [apiStatus, setApiStatus] = useState({isLoaded: false, error: null});
+  useEffect(() => {
+    const url = 'https://wfc-2019.firebaseapp.com/images?'
+      + 'limit=' + String(getRange.limit)
+      + 'offset' + String(getRange.offset);
+    fetch(url)
+      .then(response => response.json())
+      .then(response => {
+          if(response.ok) {
+            setImagesList(response.data.images);
+            setApiStatus({...apiStatus, isLoaded: true});
+          } else {
+            console.log('response is not ok.')
+          }
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
         // exceptions from actual bugs in components.
         (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
+          setApiStatus({...apiStatus, isLoaded: true, error: error});
         }
       )
-  }
-
-  render() {
-    const { error, isLoaded, images } = this.state;
-    if (error) {
-      return <div>Error: {error.message}</div>;
-    } else if (!isLoaded) {
-      return <div>Loading...</div>;
-    } else {
-      return (
-        <BrowserRouter>
-          <div>
-            <div className='photos-wrap'>
-              {images.map(image => (
-                <div className='item' key={image.id}>
-                  <Link to={`/detail/${image.id}`}>
-                    <img src={image.url} width='100%' />
-                  </Link>
-                </div>
-              ))}
-            </div>
-
-            <Route path='/detail/:id' component={PhotoDetail} />
-          </div>
-        </BrowserRouter>
-      );
-    }
+  }, getRange)
+  
+  if (apiStatus.error) {
+    return <div>Error: {apiStatus.error.message}</div>;
+  } else if (!apiStatus.isLoaded) {
+    return <div>Loading...</div>;
+  } else {
+    return (
+      <BrowserRouter>
+        <div>
+          <PhotoList images={imagesList} />
+          <Route path='/detail/:id' component={PhotoDetail} />
+        </div>
+      </BrowserRouter>
+    );
   }
 }
+
 export default App;
--- src/PhotoList.js
+++ src/PhotoList.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 const PhotoList = ({images}) => (
   <div className='photos-wrap'>
     {images.map(image => (
       <div className='item' key={image.id}>
-        <Link to={`/detail/${images.id}`}>
+        <Link to={`/detail/${image.id}`}>
           <img src={image.url} width='100%' />
         </Link>
       </div>
