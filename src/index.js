2350910fad525b69846dcbbac817d96a836c5322
honai <29170100+honai@users.noreply.github.com>
2019-03-10 16:38:13 +0900

    ポリフィル

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import 'intersection-observer';
 
 ReactDOM.render(<App />, document.getElementById('root'));
