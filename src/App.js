98f6e1aadd7f24ca79ee39746893776b8128baa7
honai <29170100+honai@users.noreply.github.com>
2019-03-18 17:14:33 +0900

    use async/await to fetch

--- src/App.js
+++ src/App.js
@@ -12,26 +12,31 @@ const App = () => {
   const [apiStatus, setApiStatus] = useState({isLoaded: false, error: false});
   const [imagesList, setImagesList] = useState([]);
   const [currentDetailStatus, setCurrentDetailStatus] = useState({id: null, imageInfo: {}});
+
   useEffect(() => {
-    const url = 'https://wfc-2019.firebaseapp.com/images?'
-      + 'offset=' + String(listSize.before)
-      + '&limit=' + String(listSize.current - listSize.before);
-    console.log('started fetch');
-    fetch(url)
-      .then(response => response.json())
-      .then(response => {
-        if(response.ok) {
-          setImagesList([...imagesList, ...response.data.images]);
+    const fetchImageList = async () => {
+      const url = 'https://wfc-2019.firebaseapp.com/images?'
+        + 'offset=' + String(listSize.before)
+        + '&limit=' + String(listSize.current - listSize.before);
+      console.log('started fetch');
+      try {
+        const response = await fetch(url);
+        console.log(response);
+        const jsonRes = await response.json()
+        console.log(jsonRes);
+        if(jsonRes.ok) {
+          setImagesList([...imagesList, ...jsonRes.data.images]);
           setApiStatus({...apiStatus, isLoaded: true, error: false});
         } else {
           setApiStatus({...apiStatus, isLoaded: true, error: true});
           console.log('response is not ok.');
         }
-      },
-      (error) => {
+      } catch (e) {
         setApiStatus({...apiStatus, isLoaded: true, error: true});
-        console.log(error);
-      });
+        console.log(e);
+      }
+    }
+    fetchImageList();
   }, [listSize]);
 
   function onLinkClick(id) {
